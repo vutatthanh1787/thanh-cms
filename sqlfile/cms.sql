840f8580d0cbbcad45a21928f33e00125bf3024a
DESKTOP-8GDN7I3\thanh <thanhvutat87@gmail.com>
2017-11-20 15:51:43 +0700

    1

--- sqlfile/cms.sql
+++ sqlfile/cms.sql
@@ -1,20 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jun 11, 2017 at 06:23 PM
--- Server version: 5.6.26
--- PHP Version: 5.5.28
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cms`
